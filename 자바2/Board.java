4d6298fdd4c19b6f5dc65d104ef66524761eb84c
seonghuncode <gnos456@naver.com>
2022-01-20 18:51:30 +0900

    데이터 구조화 하기

--- 자바2/Board.java
+++ 자바2/Board.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 public class Board {
 	
 	Scanner sc = new Scanner(System.in);
-	ArrayList<String> All_title = new ArrayList<>();
-	ArrayList<String> All_body = new ArrayList<>();
-	ArrayList<Integer> numbers = new ArrayList<>();
+	ArrayList<Collect> collects = new ArrayList<>();
+	
 	
 	int count_numbers = 1;
 	
@@ -17,10 +16,8 @@ public class Board {
 	public void runboard(){
 		
 		//자바2 연습하기----------------------------------------------------------------------------------------------------
-		Scanner sc = new Scanner(System.in);
-		
-		
 	
+		//class만들어서 구조화 했음 -> 데이터구조화 까지 하고 프로젝트 새로 만들어서 데이터 구조화 부분 한번더 하고 넘어가기!!
 		
 		while(true) {
 			
@@ -28,108 +25,24 @@ public class Board {
 			String cmd = sc.nextLine();
 			
 			if(cmd.equals("help")) {
-				System.out.println("================");
-				System.out.println("add  : 게시물 등록");
-				System.out.println("list : 게시물 목록 조회");
-				System.out.println("update : 게시물 수정");
-				System.out.println("delete : 게시물 삭제");
+				print_help();
 			}
 			else if(cmd.equals("add")) {
-				numbers.add(count_numbers);
-				
-				System.out.print("제목을 입력해 주세요 :");
-				String title = sc.nextLine();
-				All_title.add(title);
-				
-				System.out.print("내용을 입력해 주세요 :");
-				String body = sc.nextLine();
-				All_body.add(body);
-				
-				System.out.println("게시물 저장 되었습니다.");
-				count_numbers++;
+				print_add();
 			}
 			else if(cmd.equals("list")) {
-				list(All_title, All_body);
+				list();
 			}
 			else if(cmd.equals("update")) {
-				
-				while(true) {
-					
-					list(All_title, All_body);
-					
-					System.out.print("수정할 게시물 번호 :");
-					int target = Integer.parseInt(sc.nextLine());
-					
-					int target_real_num = check_exist(target);
-					
-					if(target_real_num == -1) {
-						System.out.println("없는 게시물 입니다. 다시 입력해 주세요.");
-					}
-					else {
-						
-						System.out.print("새제목 :");
-						String new_title = sc.nextLine();
-						All_title.set((target - 1), new_title);
-						
-						System.out.print("새내용 :"); 
-						String new_body = sc.nextLine();
-						All_body.set((target - 1), new_body);
-						
-						System.out.println("수정이 완료 되었습니다.");
-						list(All_title, All_body);
-						break;
-					}
-				 }
-					
+				print_update();
 				}
 			else if(cmd.equals("delete")) {
-				
-				while(true) {
-					
-					list(All_title, All_body);
-					System.out.print("삭제할 게시물 번호 : ");
-					int target = Integer.parseInt(sc.nextLine());
-					
-//					int target_real_num = -1;
-//					for(int i = 0; i < numbers.size(); i++) {
-//						int current_num = numbers.get(i); //numbers에 들어가 있는 번호 1,2,3..을 가지고 비교한다.
-//						if(current_num == delete) {
-//							target_real_num = i;
-//							break;
-//						}
-//					}
-					
-					int target_real_num = check_exist(target);
-					
-					if(target_real_num == -1) {
-						System.out.println("없는 게시물 번호 입니다. 다시 입력해 주세요.");
-					}
-					else {
-						All_title.remove(target_real_num);
-						All_body.remove(target_real_num);
-						
-						numbers.remove(target_real_num);
-						count_numbers--;
-						
-						System.out.println("게시물이 삭제 되었습니다.");
-						list(All_title, All_body);
-						break;
-					}
-				}
+				print_delete();
 			}
 				
 						
 				
-					
-					
-				
-				
-			
-			
-			
-			
-			
-			
+
 			
 		}// -> while문
 		
@@ -139,11 +52,98 @@ public class Board {
 
 	} //---------------------------------------------------------------------------->public class
 	
+	private void print_delete() {
+		while(true) {
+			
+			list();
+			System.out.print("삭제할 게시물 번호 : ");
+			int target = Integer.parseInt(sc.nextLine());
+			
+//			int target_real_num = -1;
+//			for(int i = 0; i < numbers.size(); i++) {
+//				int current_num = numbers.get(i); //numbers에 들어가 있는 번호 1,2,3..을 가지고 비교한다.
+//				if(current_num == delete) {
+//					target_real_num = i;
+//					break;
+//				}
+//			}
+			
+			int target_real_num = check_exist(target);
+			
+			if(target_real_num == -1) {
+				System.out.println("없는 게시물 번호 입니다. 다시 입력해 주세요.");
+			}
+			else {
+				
+				collects.remove(target_real_num);
+				
+				System.out.println("게시물이 삭제 되었습니다.");
+				list();
+				break;
+			}
+		}
+	}
+
+	private void print_update() {
+		
+		while(true) {
+			
+			list();
+			
+			System.out.print("수정할 게시물 번호 :");
+			int target = Integer.parseInt(sc.nextLine());
+			
+			int target_real_num = check_exist(target);
+			
+			if(target_real_num == -1) {
+				System.out.println("없는 게시물 입니다. 다시 입력해 주세요.");
+			}
+			else {
+				
+				System.out.print("새제목 :");
+				String new_title = sc.nextLine();
+				
+				System.out.print("새내용 :"); 
+				String new_body = sc.nextLine();
+			
+				Collect collect = new Collect(target, new_title, new_body);
+				collects.set(target_real_num, collect);
+				
+				System.out.println("수정이 완료 되었습니다.");
+				list();
+				break;
+			}
+		 }
+	}
+
+	private void print_add() {
+		System.out.print("제목을 입력해 주세요 :");
+		String title = sc.nextLine();
+		
+		System.out.print("내용을 입력해 주세요 :");
+		String body = sc.nextLine();
+		
+		Collect collect = new Collect(count_numbers, title, body);// --> class의 인스턴스를 만들어 사용한다.
+		collects.add(collect); // --> collects의 배열에 collect를 저장해 준다.
+		
+		System.out.println("게시물 저장 되었습니다.");
+		count_numbers++;
+	}
+
+	private void print_help() {
+		System.out.println("================");
+		System.out.println("add  : 게시물 등록");
+		System.out.println("list : 게시물 목록 조회");
+		System.out.println("update : 게시물 수정");
+		System.out.println("delete : 게시물 삭제");
+	}
+
 	public int check_exist(int target) {
 		
-		for(int i = 0; i < numbers.size(); i++) {
-			int current_num = numbers.get(i);
-			if(target == current_num) {
+		for(int i = 0; i < collects.size(); i++) {
+			//int current_num = collects.get(i).id;
+			Collect current_num = collects.get(i);
+			if(target == current_num.id) {
 				return i;
 			}
 		}
@@ -152,11 +152,14 @@ public class Board {
 		
 		
 	
-	public static void list(ArrayList<String> All_title, ArrayList<String> All_body) {
-		for(int i = 0; i < All_title.size(); i++) {
-			System.out.println("번호 :" + (i + 1));
-			System.out.println("제목 :" + All_title.get(i));
-			System.out.println("내용 :" + All_body.get(i)); 
+	public void list() {
+		for(int i = 0; i < collects.size(); i++) {
+			
+			Collect collect = collects.get(i);
+			
+			System.out.println("번호 :" + collect.id);
+			System.out.println("제목 :" + collect.title);
+			System.out.println("내용 :" + collect.body); 
 			System.out.println("===================");
 		}
 	}
---
+++ 자바2/Collect.java
@@ -0,0 +1,15 @@
+package 자바2;
+
+public class Collect {
+
+	int id;
+	String title;
+	String body;
+	
+	public Collect(int id, String title, String body) {  //생성자를 사용하기 위해 만들어 주어야 한다.
+		this.id = id; 								//class명과 똑깉은 이름으로 사용해야 한다.
+		this.title = title;
+		this.body = body;
+	}
+	
+}
